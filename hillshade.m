5c26448d6ef9f4247c458fd5aa801677c4f22558
williameclee <williameclee@gmail.com>
2023-09-22 16:56:20 -0700

    Improved efficiency and eligibility

--- hillshade.m
+++ hillshade.m
@@ -1,4 +1,4 @@
-function [HS, light_dir, flat_reflection] = hillshade(Z, varargin)
+function [HS, lvector, flat_reflection] = hillshade(Z, varargin)
     % HILLSHADE calculate hillshade of a DEM
     % Syntax
     %   HS = hillshade(Z)
@@ -6,7 +6,7 @@ function [HS, light_dir, flat_reflection] = hillshade(Z, varargin)
     %       "azimuth", az_deg, "altitude", al_deg, ...
     %       "z_factor", ex_fac)
     %   HS = hillshade(Z, "Dx", Dx, ...
-    %       "light_direction", light_dir, ...
+    %       "light_vector", lvector, ...
     %       "z_factor", ex_fac)
     % Input
     %   Z: DEM matrix in metres. The matrix index should be in the (j,i) format, as of mdgrid
@@ -14,19 +14,19 @@ function [HS, light_dir, flat_reflection] = hillshade(Z, varargin)
     %   "Dx": Grid spacing in metres (default = 1)
     %   "azimuth": Azimuth angle in degrees (default = 45)
     %   "altitude": Altitude angle in degrees (default = 45)
-    %   "light_direction": Light direction vector (default = [0,0,0])
+    %   "light_vector": Light direction vector (default = [0,0,0])
     %   "z_factor": Vertical exaggeration factor (default = 1)
-    % Either "azimuth" and "altitude" or "light_direction" should be specified.
+    % Either "azimuth" and "altitude" or "light_vector" should be specified.
     % Output
     %   HS: Hillshade matrix, with the same size as Z
-    %   light_dir: Light direction vector used in the calculation
+    %   lvector: Light direction vector used in the calculation
     %   flat_reflection: Hillshade value of a flat surface
 
     % Assigning default parameters
     Dx = 1;
     az = 45;
     al = 45;
-    light_dir = [0, 0, 0];
+    lvector = [0, 0, 0];
     ex_fac = 1;
 
     % Input parameters
@@ -35,37 +35,38 @@ function [HS, light_dir, flat_reflection] = hillshade(Z, varargin)
     addOptional(p, "Dx", Dx);
     addOptional(p, "azimuth", az);
     addOptional(p, "altitude", al);
-    addOptional(p, "light_direction", light_dir);
+    addOptional(p, "light_vector", lvector);
     addOptional(p, "z_factor", ex_fac);
     parse(p, Z, varargin{:});
     Z = p.Results.Z;
     Dx = p.Results.Dx;
     az = p.Results.azimuth;
     al = p.Results.altitude;
-    light_dir = p.Results.light_direction;
+    lvector = p.Results.light_vector;
     ex_fac = p.Results.z_factor;
 
     % Parameters cleaning
     Z_ex = Z * ex_fac / Dx;
 
-    if norm(light_dir(:)) == 0
-        light_dir = light_dir_calc(az, al);
+    if norm(lvector(:)) == 0
+        lvector = lvector_calc(az, al);
     else
-        light_dir = light_dir / sum(light_dir .^ 2);
+        lvector = lvector / norm(lvector(:));
     end
 
-    light_dir = light_dir * sign(light_dir(3));
-    light_dir = reshape(light_dir, [1, 1, 3]);
-    flat_reflection = light_dir(3);
+    lvector = lvector * sign(lvector(3));
+    lvector = reshape(lvector, [1, 1, 3]);
+    flat_reflection = lvector(3);
 
     % Calculate hillshade
     N = zeros([size(Z_ex), 3]);
     [N(:, :, 1), N(:, :, 2), N(:, :, 3)] = surfnorm(Z_ex);
-    Light_dir = repmat(light_dir, [size(Z_ex)]);
+    Light_dir = repmat(lvector, [size(Z_ex)]);
     HS = sum(Light_dir .* N, 3);
     HS = max(HS, 0);
 end
 
-function light_dir = light_dir_calc(az, al)
-    light_dir = [sind(az) * cosd(al), cosd(az) * cosd(al), sind(al)];
+function lvector = lvector_calc(az, al)
+    lvector = [sind(az) * cosd(al), cosd(az) * cosd(al), sind(al)];
+    lvector = lvector / norm(lvector(:));
 end
--- shadow.m
+++ shadow.m
@@ -6,7 +6,7 @@ function Z_dp = shadow(Z, varargin)
     %       "azimuth", az, "altitude", al, ...
     %       "z_factor", ex_fac)
     %   HS = hillshade(Z, "Dx", Dx, ...
-    %       "light_direction", light_dir, ...
+    %       "light_vector", lvector, ...
     %       "z_factor", ex_fac)
     % Input
     %   Z: DEM matrix in metres. The matrix index should be in the (j,i) format, as of mdgrid
@@ -14,9 +14,9 @@ function Z_dp = shadow(Z, varargin)
     %   "Dx": Grid spacing in metres (default = 1)
     %   "azimuth": Azimuth angle in degrees (default = 45)
     %   "altitude": Altitude angle in degrees (default = 45)
-    %   "light_direction": Light direction vector (default = [0,0,0])
+    %   "light_vector": Light direction vector (default = [0,0,0])
     %   "z_factor": Vertical exaggeration factor (default = 1)
-    % Either "azimuth" and "altitude" or "light_direction" should be specified.
+    % Either "azimuth" and "altitude" or "light_vector" should be specified.
     % Output
     %   Z_dp: Depth of shadow casted on the DEM
 
@@ -24,9 +24,9 @@ function Z_dp = shadow(Z, varargin)
     Dx = 1;
     az = 45;
     al = 45;
-    light_dir = [0, 0, 0];
+    lvector = [0, 0, 0];
     ex_fac = 1;
-    quality = "dirty";
+    algorithm = "dirty";
 
     % Input parameters
     p = inputParser;
@@ -34,58 +34,62 @@ function Z_dp = shadow(Z, varargin)
     addOptional(p, "Dx", Dx);
     addOptional(p, "azimuth", az);
     addOptional(p, "altitude", al);
-    addOptional(p, "light_direction", light_dir);
+    addOptional(p, "light_vector", lvector);
     addOptional(p, "z_factor", ex_fac);
-    addOptional(p, "quality", quality);
+    addOptional(p, "quality", algorithm);
     parse(p, Z, varargin{:});
     Z = p.Results.Z;
     Dx = p.Results.Dx;
     az = p.Results.azimuth;
     al = p.Results.altitude;
-    light_dir = p.Results.light_direction;
+    lvector = p.Results.light_vector;
     ex_fac = p.Results.z_factor;
-    quality = p.Results.quality;
+    algorithm = p.Results.quality;
 
     % Parameters cleaning
     Z_ex = Z * ex_fac / Dx;
 
-    if norm(light_dir(:)) ~= 0
-        al = asind(light_dir(3) / norm(light_dir));
-        az = atan2d(light_dir(2), light_dir(1));
+    if norm(lvector(:)) ~= 0
+        al = asind(lvector(3) / norm(lvector));
+        az = atan2d(lvector(2), lvector(1));
     end
 
     % Prepare and rotate DEM
-    [Z_exp, I_exp, J_exp] = shadow_rotatefwd(Z_ex, az, quality);
+    [Z_exp, I_exp, J_exp] = shadow_rotatefwd(Z_ex, az, algorithm);
 
     Z_slp = Z_exp - tand(al) * J_exp;
     Z_max = cummax(Z_slp, 1, "reverse", "omitnan");
     Z_dpth = Z_slp - Z_max;
 
-    Z_dp = shadow_rotatebwd(I_exp, J_exp, Z_dpth, az, size(Z), quality);
+    Z_dp = shadow_rotatebwd(I_exp, J_exp, Z_dpth, az, size(Z), algorithm);
 end
 
-function [Z_exp, I_exp, J_exp] = shadow_rotatefwd(Z, az, quality)
+%% Supplementary functions
+% Rotate DEM
+function [Z_exp, I_exp, J_exp] = shadow_rotatefwd(Z, az, algorithm)
     Z_pad = padarray(Z, [1, 1], 'replicate');
     [I_pad, J_pad] = meshgrid(1:size(Z_pad, 2), 1:size(Z_pad, 1));
-    N = size(Z_pad, 2);
-    M = size(Z_pad, 1);
+    N_pad = size(Z_pad, 2);
+    M_pad = size(Z_pad, 1);
     RM = [cosd(az), -sind(az); sind(az), cosd(az)];
-    C1 = RM * [1; 1];
-    C2 = RM * [1; M];
-    C3 = RM * [N; 1];
-    C4 = RM * [N; M];
+    C1_rot = RM * [1; 1];
+    C2_rot = RM * [1; M_pad];
+    C3_rot = RM * [N_pad; 1];
+    C4_rot = RM * [N_pad; M_pad];
 
-    Cx = [C1(1), C2(1), C3(1), C4(1)];
-    Cy = [C1(2), C2(2), C3(2), C4(2)];
-    [I_exp, J_exp] = meshgrid(floor(min(Cx)):ceil(max(Cx)), ...
-        floor(min(Cy)):ceil(max(Cy)));
+    Cx_rot = [C1_rot(1), C2_rot(1), C3_rot(1), C4_rot(1)];
+    Cy_rot = [C1_rot(2), C2_rot(2), C3_rot(2), C4_rot(2)];
+    [I_exp, J_exp] = meshgrid(floor(min(Cx_rot)):ceil(max(Cx_rot)), ...
+        floor(min(Cy_rot)):ceil(max(Cy_rot)));
 
     IJ_rot = RM * [I_pad(:)'; J_pad(:)'];
     I_rot = IJ_rot(1, :).';
     J_rot = IJ_rot(2, :).';
 
+    clear IJ_rot I_pad J_pad C1_rot C2_rot C3_rot C4_rot
+
     % Interpolate DEM
-    switch quality
+    switch algorithm
         case "high"
             Z_exp = griddata(I_rot, J_rot, Z_pad(:), I_exp, J_exp);
         case "dirty"
@@ -93,16 +97,17 @@ function [Z_exp, I_exp, J_exp] = shadow_rotatefwd(Z, az, quality)
             J_rot = reshape(J_rot, size(Z_pad));
             Z_exp = zeros(size(I_exp));
             W = zeros(size(Z_exp));
-
-            iofst = 1 - floor(min(Cx));
-            jofst = 1 - floor(min(Cy));
+            
+            % offset between rotated and expended grids
+            ifst = 1 - floor(min(Cx_rot));
+            jfst = 1 - floor(min(Cy_rot));
 
             Ifl = floor(I_rot);
             Jfl = floor(J_rot);
             T = I_rot - Ifl;
             S = J_rot - Jfl;
-            Ifl = Ifl + iofst;
-            Jfl = Jfl + jofst;
+            Ifl = Ifl + ifst;
+            Jfl = Jfl + jfst;
             Z_exp(sub2ind(size(Z_exp), Jfl, Ifl)) ...
                 = Z_exp(sub2ind(size(Z_exp), Jfl, Ifl)) ...
                 + (1 - T) .* (1 - S) .* Z_pad;
@@ -134,71 +139,76 @@ function [Z_exp, I_exp, J_exp] = shadow_rotatefwd(Z, az, quality)
 
 end
 
-function Z_dp = shadow_rotatebwd(I_exp, J_exp, Z_dpth, az, MN, quality)
+% Rotate DEM back
+function Z = shadow_rotatebwd(I_exp, J_exp, Z_exp, az, MN, algorithm)
     N = MN(2);
     M = MN(1);
     RM_b = [cosd(-az), -sind(-az); sind(-az), cosd(-az)];
 
     I_exp = I_exp(:);
     J_exp = J_exp(:);
-    Z_dpth = Z_dpth(:);
-    I_exp = I_exp(~isnan(Z_dpth));
-    J_exp = J_exp(~isnan(Z_dpth));
-    Z_dpth = Z_dpth(~isnan(Z_dpth));
+    Z_exp = Z_exp(:);
+    I_exp = I_exp(~isnan(Z_exp));
+    J_exp = J_exp(~isnan(Z_exp));
+    Z_exp = Z_exp(~isnan(Z_exp));
 
     IJ_rotb = RM_b * [I_exp'; J_exp'];
     I_exp = IJ_rotb(1, :).';
     J_exp = IJ_rotb(2, :).';
 
+    clear IJ_rotb
+
     % Interpolate DEM
-    switch quality
+    switch algorithm
         case "high"
-            [I, J] = meshgrid(2:N + 1, 2:M + 1);
-            Z_dp = griddata(I_exp, J_exp, Z_dpth, I, J);
+            [I, J] = meshgrid(1:N, 1:M);
+            Z = griddata(I_exp - 1, J_exp - 1, Z_exp, I, J);
         case "dirty"
-            Z_dp = zeros(MN);
+            Z = zeros(MN);
             W = zeros(MN);
 
-            Ifl = floor(I_exp);
-            Jfl = floor(J_exp);
-            T = I_exp - Ifl;
-            S = J_exp - Jfl;
+            Ifl = floor(I_exp) - 1;
+            Jfl = floor(J_exp) - 1;
+            T = (I_exp - 1) - Ifl;
+            S = (J_exp - 1) - Jfl;
 
-            BL = (Ifl >= 1) & (Jfl >= 1) & (Ifl <= N) & (Jfl <= M);
-            BR = (Ifl + 1 >= 1) & (Jfl >= 1) & (Ifl + 1 <= N) & (Jfl <= M);
-            TL = (Ifl >= 1) & (Jfl + 1 >= 1) & (Ifl <= N) & (Jfl + 1 <= M);
-            TR = (Ifl + 1 >= 1) & (Jfl + 1 >= 1) & (Ifl + 1 <= N) & (Jfl + 1 <= M);
+            BL = (Ifl >= 1) & (Jfl >= 1) ...
+                & (Ifl <= N) & (Jfl <= M);
+            BR = (Ifl + 1 >= 1) & (Jfl >= 1) ...
+                & (Ifl + 1 <= N) & (Jfl <= M);
+            TL = (Ifl >= 1) & (Jfl + 1 >= 1) ...
+                & (Ifl <= N) & (Jfl + 1 <= M);
+            TR = (Ifl + 1 >= 1) & (Jfl + 1 >= 1) ...
+                & (Ifl + 1 <= N) & (Jfl + 1 <= M);
 
-            Z_dp(sub2ind(size(Z_dp), Jfl(BL), Ifl(BL))) ...
-                = Z_dp(sub2ind(size(Z_dp), Jfl(BL), Ifl(BL))) ...
-                + (1 - T(BL)) .* (1 - S(BL)) .* Z_dpth(BL);
-            Z_dp(sub2ind(size(Z_dp), Jfl(TL) + 1, Ifl(TL))) ...
-                = Z_dp(sub2ind(size(Z_dp), Jfl(TL) + 1, Ifl(TL))) ...
-                + (1 - T(TL)) .* S(TL) .* Z_dpth(TL);
-            Z_dp(sub2ind(size(Z_dp), Jfl(BR), Ifl(BR) + 1)) ...
-                = Z_dp(sub2ind(size(Z_dp), Jfl(BR), Ifl(BR) + 1)) ...
-                + T(BR) .* (1 - S(BR)) .* Z_dpth(BR);
-            Z_dp(sub2ind(size(Z_dp), Jfl(TR) + 1, Ifl(TR) + 1)) ...
-                = Z_dp(sub2ind(size(Z_dp), Jfl(TR) + 1, Ifl(TR) + 1)) ...
-                + T(TR) .* S(TR) .* Z_dpth(TR);
+            Z(sub2ind(size(Z), Jfl(BL), Ifl(BL))) ...
+                = Z(sub2ind(size(Z), Jfl(BL), Ifl(BL))) ...
+                + (1 - T(BL)) .* (1 - S(BL)) .* Z_exp(BL);
+            Z(sub2ind(size(Z), Jfl(TL) + 1, Ifl(TL))) ...
+                = Z(sub2ind(size(Z), Jfl(TL) + 1, Ifl(TL))) ...
+                + (1 - T(TL)) .* S(TL) .* Z_exp(TL);
+            Z(sub2ind(size(Z), Jfl(BR), Ifl(BR) + 1)) ...
+                = Z(sub2ind(size(Z), Jfl(BR), Ifl(BR) + 1)) ...
+                + T(BR) .* (1 - S(BR)) .* Z_exp(BR);
+            Z(sub2ind(size(Z), Jfl(TR) + 1, Ifl(TR) + 1)) ...
+                = Z(sub2ind(size(Z), Jfl(TR) + 1, Ifl(TR) + 1)) ...
+                + T(TR) .* S(TR) .* Z_exp(TR);
 
-            W(sub2ind(size(Z_dp), Jfl(BL), Ifl(BL))) ...
-                = W(sub2ind(size(Z_dp), Jfl(BL), Ifl(BL))) ...
+            W(sub2ind(size(Z), Jfl(BL), Ifl(BL))) ...
+                = W(sub2ind(size(Z), Jfl(BL), Ifl(BL))) ...
                 + (1 - T(BL)) .* (1 - S(BL));
-            W(sub2ind(size(Z_dp), Jfl(TL) + 1, Ifl(TL))) ...
-                = W(sub2ind(size(Z_dp), Jfl(TL) + 1, Ifl(TL))) ...
+            W(sub2ind(size(Z), Jfl(TL) + 1, Ifl(TL))) ...
+                = W(sub2ind(size(Z), Jfl(TL) + 1, Ifl(TL))) ...
                 + (1 - T(TL)) .* S(TL);
-            W(sub2ind(size(Z_dp), Jfl(BR), Ifl(BR) + 1)) ...
-                = W(sub2ind(size(Z_dp), Jfl(BR), Ifl(BR) + 1)) ...
+            W(sub2ind(size(Z), Jfl(BR), Ifl(BR) + 1)) ...
+                = W(sub2ind(size(Z), Jfl(BR), Ifl(BR) + 1)) ...
                 + T(BR) .* (1 - S(BR));
-            W(sub2ind(size(Z_dp), Jfl(TR) + 1, Ifl(TR) + 1)) ...
-                = W(sub2ind(size(Z_dp), Jfl(TR) + 1, Ifl(TR) + 1)) ...
+            W(sub2ind(size(Z), Jfl(TR) + 1, Ifl(TR) + 1)) ...
+                = W(sub2ind(size(Z), Jfl(TR) + 1, Ifl(TR) + 1)) ...
                 + T(TR) .* S(TR);
 
-            Z_dp(W ~= 0) = Z_dp(W ~= 0) ./ W(W ~= 0);
-            Z_dp(W == 0) = nan;
-
-            % Z_dp = Z_dp(2:end - 1, 2:end - 1);
+            Z(W ~= 0) = Z(W ~= 0) ./ W(W ~= 0);
+            Z(W == 0) = nan;
     end
 
 end
